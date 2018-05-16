c3f17f304e0c9b8b6e1965054512b1dd1a858a41
Earthcomputer <burtonjae@hotmail.co.uk>
2018-05-16 20:08:32 +0100

    Remove dispenser and lava advancers

--- src/net/earthcomputer/lightningtool/RNGAdvancer.java
+++ src/net/earthcomputer/lightningtool/RNGAdvancer.java
@@ -10,29 +10,6 @@ public enum RNGAdvancer {
 		public void advance(Random rand) {
 			rand.nextInt();
 		}
-	},
-	DISPENSER {
-		@Override
-		public void advance(Random rand) {
-			rand.nextLong();
-			rand.nextGaussian();
-			rand.nextGaussian();
-			rand.nextGaussian();
-		}
-	},
-	LAVA {
-		@Override
-		public void advance(Random rand) {
-			for (int i = 0; i < 3; i++) {
-				int times = rand.nextInt(3);
-				if (times == 0)
-					times = 3;
-				for (int j = 0; j < times; j++) {
-					rand.nextInt(3);
-					rand.nextInt(3);
-				}
-			}
-		}
 	};
 	// @formatter:on
 
